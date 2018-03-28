521319b5ce974d0571d2eb9443d8c841b75f44bb
Arpit Gupta <guptarpit1997@gmail.com>
2018-03-28 21:15:56 +0530

    Add models Snippet and Comment, makemigrations

--- snippets/models.py
+++ snippets/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Snippet(models.Model):
+	title = models.CharField(max_length = 30)
+	content = models.TextField()
+	datetime_created = models.DateTimeField(auto_now = True)
+
+
+class Comment(models.Model):
+	snippet = models.ForeignKey(to = 'Snippet', on_delete = models.CASCADE)
+	content = models.TextField()
+	datetime_created = models.DateTimeField(auto_now = True)
