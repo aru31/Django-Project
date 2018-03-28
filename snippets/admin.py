b8b70c8473f903bdcef06588581f85ed1dd763ca
Arpit Gupta <guptarpit1997@gmail.com>
2018-03-28 21:46:56 +0530

    Modified admin_page

--- snippets/admin.py
+++ snippets/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
-# Register your models here.
+from snippets.models import Snippet, Comment
+admin.site.register(Snippet)
+admin.site.register(Comment)
--- snippets/models.py
+++ snippets/models.py
@@ -8,8 +8,13 @@ class Snippet(models.Model):
 	content = models.TextField()
 	datetime_created = models.DateTimeField(auto_now = True)
 
+	def __str__(self):
+		return self.title
 
 class Comment(models.Model):
 	snippet = models.ForeignKey(to = 'Snippet', on_delete = models.CASCADE)
 	content = models.TextField()
 	datetime_created = models.DateTimeField(auto_now = True)
+
+	def __str__(self):
+		return "{0}: {1}".format(str(self.snippet), self.content)
