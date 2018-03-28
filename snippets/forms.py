ce4c07672b9fb7a318f59b51ada98c97d6a04b54
Arpit Gupta <guptarpit1997@gmail.com>
2018-03-29 00:18:14 +0530

    pastebin ready

--- snippets/forms.py
+++ snippets/forms.py
@@ -1,5 +1,11 @@
 from django import forms
+from snippets.models import Comment
 
 class SnippetForm(forms.Form):
 	title = forms.CharField(label = 'Title', max_length=30)
 	content = forms.CharField(label = 'Content', widget = forms.Textarea)
+
+class CommentForm(forms.ModelForm):
+	class Meta:
+		model = Comment
+		fields = ['content']
--- snippets/views.py
+++ snippets/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404
-from snippets.forms import SnippetForm
+from snippets.forms import SnippetForm, CommentForm
 from snippets.models import Snippet
 
 def create(request):
@@ -16,11 +16,21 @@ def create(request):
 	return render(request, 'snippets/create.html', {'form': form})
 
 def view(request, key):
-	if request.method == "GET":
-		if Snippet.objects.filter(id = key).exists():
-			snippet = Snippet.objects.get(id = key)
-		else:
-			raise Http404("Snippet Doesnot Exist")
-	return render(request, 'snippets/view.html', {'key': key, 'title': snippet.title, 'content': snippet.content} )
+	if Snippet.objects.filter(id = key).exists():
+		snippet = Snippet.objects.get(id = key)
+	else:
+		raise Http404("Snippet Doesnot Exist")
+
+	if request.method =="POST":
+		form = CommentForm(request.POST)
+		if form.is_valid():
+			comment = form.save(commit = False)
+			comment.snippet = snippet
+			comment.save()
+	form = CommentForm()
+	comments = snippet.comment_set.order_by('-datetime_created')
+
+
+	return render(request, 'snippets/view.html', {'key': key, 'title': snippet.title, 'content': snippet.content, 'form': form, 'comments': comments} )
 
 
